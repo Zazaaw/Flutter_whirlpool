34baca7e7903e466924114090f34931916df659c
Zazaaw <faizandhilmi@gmail.com>
2023-11-09 14:46:01 +0700

    (main_view_model.dart) Reduce timer of the item models

--- lib/view_models/main_view_model.dart
+++ lib/view_models/main_view_model.dart
@@ -22,17 +22,17 @@ class MainViewModel with ChangeNotifier {
   List<ModeItemModel> nodes = const [
     ModeItemModel(
       name: 'Standard',
-      minutes: 32,
+      minutes: 15,
       color: Color.fromRGBO(61, 111, 252, 1),
     ),
     ModeItemModel(
       name: 'Gentle',
-      minutes: 24,
+      minutes: 7,
       color: Color.fromRGBO(50, 197, 253, 1),
     ),
     ModeItemModel(
       name: 'Fast',
-      minutes: 12,
+      minutes: 1,
       color: Color.fromRGBO(253, 133, 53, 1),
     ),
   ];
